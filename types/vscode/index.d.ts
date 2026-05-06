83e302af0520b080363094af8ae71ffa8c61e230
Robo <hop2deep@gmail.com>
2026-05-06 16:43:48 +0900

    chore: update vscode types for v1.119

--- types/vscode/index.d.ts
+++ types/vscode/index.d.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 /**
- * Type Definition for Visual Studio Code 1.118 Extension API
+ * Type Definition for Visual Studio Code 1.119 Extension API
  * See https://code.visualstudio.com/api for more information
  */
 
